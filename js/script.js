c5cf0a9114c0228fab22ef3fa09489aa0a03fd2e
Harshad <hgillurk@its.jnj.com>
2021-07-25 00:30:11 +0800

    home page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n,divName,dots) {
+  showDivs(slideIndex += n, divName, dots);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentDiv(n, divName) {
+  showDivs(slideIndex = n, divName);
 }
 
-function showDivs(n) {
+function showDivs(n, divName, dotsId) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots;
+  var x = document.getElementsByClassName(divName);
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
   if(slideIndex){
         x[slideIndex-1].style.display = "block";  
         x[slideIndex-1].style.width = "100%";  
         x[slideIndex-1].style.height = "100%";  
-        dots[slideIndex-1].className += " w3-white";
+        x[slideIndex-1].style.opacity = "0.8";  
+  }
+  if(dotsId){
+    dots = document.getElementsByClassName(dotsId);
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-white", "");
     }
+    dots[slideIndex-1].className += " w3-white";
+  }
 }
